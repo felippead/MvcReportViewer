63954e46bcc5823411904d453d190ea4f35a7fd0
filip.adrian <filip.adrian@gmail.com>
2016-12-01 02:22:37 +0200

    add some c# 6.0 features to the code and perform a little cleanup of the code

--- AlanJuden.MvcReportViewer/ReportParameterInfo.cs
+++ AlanJuden.MvcReportViewer/ReportParameterInfo.cs
@@ -1,25 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace AlanJuden.MvcReportViewer
 {
-	public class ReportParameterInfo
-	{
-		public string Name { get; set; }
-		public string Prompt { get; set; }
-		public bool MultiValue { get; set; }
-		public Dictionary<string, string> ValidValues { get; set; }
-		public List<string> SelectedValues { get; set; }
-		public ReportService.ParameterTypeEnum Type { get; set; }
-		public bool PromptUser { get; set; }
+    public class ReportParameterInfo
+    {
+        public string Name { get; set; }
+        public string Prompt { get; set; }
+        public bool MultiValue { get; set; }
+        public Dictionary<string, string> ValidValues { get; set; }
+        public List<string> SelectedValues { get; set; }
+        public ReportService.ParameterTypeEnum Type { get; set; }
+        public bool PromptUser { get; set; }
 
-		public ReportParameterInfo()
-		{
-			this.ValidValues = new Dictionary<string, string>();
-			this.SelectedValues = new List<string>();
-		}
-	}
+        public ReportParameterInfo()
+        {
+            ValidValues = new Dictionary<string, string>();
+            SelectedValues = new List<string>();
+        }
+    }
 }
--- AlanJuden.MvcReportViewer/ReportViewerModel.cs
+++ AlanJuden.MvcReportViewer/ReportViewerModel.cs
@@ -1,83 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Web;
 
 namespace AlanJuden.MvcReportViewer
 {
-	public class ReportViewerModel
-	{
-		/// <summary>
-		/// 
-		/// </summary>
-		public string ServerUrl { get; set; }
+    public class ReportViewerModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ServerUrl { get; set; }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public string ReportPath { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ReportPath { get; set; }
 
-		/// <summary>
-		/// This indicates whether or not to replace image urls from your report server to image urls on your local site to act as a proxy
-		/// *useful if your report server is not accessible publicly*
-		/// </summary>
-		public bool UseCustomReportImagePath { get; set; }
+        /// <summary>
+        /// This indicates whether or not to replace image urls from your report server to image urls on your local site to act as a proxy
+        /// *useful if your report server is not accessible publicly*
+        /// </summary>
+        public bool UseCustomReportImagePath { get; set; }
 
-		/// <summary>
-		/// This is the local URL on your website that will handle returning images for you, be sure to use the replacement variable {0} in your string to represent the original image URL that came from your report server.
-		/// </summary>
-		public string ReportImagePath { get; set; }
+        /// <summary>
+        /// This is the local URL on your website that will handle returning images for you, be sure to use the replacement variable {0} in your string to represent the original image URL that came from your report server.
+        /// </summary>
+        public string ReportImagePath { get; set; }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public System.Net.ICredentials Credentials { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.Net.ICredentials Credentials { get; set; }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public Dictionary<string, string[]> Parameters { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Dictionary<string, string[]> Parameters { get; set; }
 
-		public bool ShowHiddenParameters { get; set; }
+        public bool ShowHiddenParameters { get; set; }
 
-		public ReportViewModes ViewMode { get; set; }
+        public ReportViewModes ViewMode { get; set; }
 
-		public ReportViewerModel()
-		{
-			this.Parameters = new Dictionary<string, string[]>();
-			this.ViewMode = ReportViewModes.View;
-		}
+        public ReportViewerModel()
+        {
+            Parameters = new Dictionary<string, string[]>();
+            ViewMode = ReportViewModes.View;
+        }
 
-		public void AddParameter(string name, string value)
-		{
-			this.AddParameter(name, new string[1] { value });
-		}
+        public void AddParameter(string name, string value)
+        {
+            AddParameter(name, new string[1] { value });
+        }
 
-		public void AddParameter(string name, string[] values)
-		{
-			if (this.Parameters.ContainsKey(name))
-			{
-				this.Parameters[name] = values;
-			}
-			else
-			{
-				this.Parameters.Add(name, values);
-			}
-		}
+        public void AddParameter(string name, string[] values)
+        {
+            if (Parameters.ContainsKey(name))
+            {
+                Parameters[name] = values;
+            }
+            else
+            {
+                Parameters.Add(name, values);
+            }
+        }
 
-		public void BuildParameters(HttpRequestBase request)
-		{
-			foreach (var key in request.QueryString.AllKeys)
-			{
-				this.AddParameter(key, request.QueryString[key].ToSafeString().ToStringList().ToArray());
-			}
+        public void BuildParameters(HttpRequestBase request)
+        {
+            foreach (var key in request.QueryString.AllKeys)
+            {
+                AddParameter(key, request.QueryString[key].ToSafeString().ToStringList().ToArray());
+            }
 
-			foreach (var key in request.Form.AllKeys)
-			{
-				this.AddParameter(key, request.Form[key].ToSafeString().ToStringList().ToArray());
-			}
-		}
-	}
+            foreach (var key in request.Form.AllKeys)
+            {
+                AddParameter(key, request.Form[key].ToSafeString().ToStringList().ToArray());
+            }
+        }
+    }
 }
